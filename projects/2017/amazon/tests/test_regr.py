6886f88a9f9b0b17c48c133cbb1517f72609531f
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-05-31 15:01:09 +0300

    Test suite elaboration

--- projects/2017/amazon/tests/test_regr.py
+++ projects/2017/amazon/tests/test_regr.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import unittest
-from amazon.miner import sample_lookup
+from miner import sample_lookup
 
 
 class InitialTest(unittest.TestCase):
     """Test how it works out from the box"""
 
-    def test_simple_query(self):
-        product = sample_lookup('B00EOE0WKQ')
-        self.assertEqual(dir(product), 86)
+    def setUp(self):
+        self.product = sample_lookup('B00EOE0WKQ')
+
+    def test_simple_query_number_properties(self):
+        self.assertEqual(len(dir(self.product)), 93)
