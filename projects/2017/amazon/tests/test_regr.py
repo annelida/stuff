55a0bee0831fe74fe0449d9ae6a6f3793d97b764
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-05-31 16:41:46 +0300

    Product properties available in Amazon scraper

--- projects/2017/amazon/tests/test_regr.py
+++ projects/2017/amazon/tests/test_regr.py
@@ -53,11 +53,19 @@ class InitialAmazonScraperTest(unittest.TestCase):
         self.assertEqual(len(dir(self.product)), 39)
 
     def test_properties_names(self):
-        names = ['alternatives', 'api', 'author_bio', 'author_page_url', 'product',
-                 'ratings', 'reviews', 'reviews_url', 'soup', 'supplemental_text',
-                 'to_dict', 'url']
+        names = ['actors', 'api', 'asin', 'author', 'authors', 'availability', 'availability_max_hours',
+                 'availability_min_hours', 'availability_type', 'aws_associate_tag', 'binding', 'brand',
+                 'browse_nodes', 'color', 'creators', 'detail_page_url', 'directors', 'ean', 'edition',
+                 'editorial_review', 'editorial_reviews', 'eisbn', 'features', 'formatted_price', 'genre',
+                 'get_attribute', 'get_attribute_details', 'get_attributes', 'get_parent', 'images',
+                 'is_adult', 'is_preorder', 'isbn', 'label', 'languages', 'large_image_url', 'list_price',
+                 'manufacturer', 'medium_image_url', 'model', 'mpn', 'offer_id', 'offer_url', 'pages',
+                 'parent', 'parent_asin', 'parsed_response', 'part_number', 'price_and_currency',
+                 'product_group', 'product_type_name', 'publication_date', 'publisher', 'region',
+                 'release_date', 'reviews', 'running_time', 'sales_rank', 'sku', 'small_image_url',
+                 'studio', 'tiny_image_url', 'title', 'to_string', 'upc']
         for name in names:
-            self.assertTrue(name in dir(self.product))
+            self.assertTrue(name in dir(self.product.product))
 
     def tearDown(self):
         del(self.product)
