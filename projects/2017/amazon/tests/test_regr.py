e91a7d9b3c99456000b116e80e7fac23f556f145
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-05-31 15:08:32 +0300

    Credentials test suite

--- projects/2017/amazon/tests/test_regr.py
+++ projects/2017/amazon/tests/test_regr.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 import unittest
 from miner import sample_lookup
+from settings import AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG
 
+class CredentialTest(unittest.TestCase):
+
+    def test_credentials_not_empty(self):
+        self.assertTrue(AMAZON_ACCESS_KEY, "Amazon access key is empty")
+        self.assertTrue(AMAZON_SECRET_KEY, "Amazon secret key is empty")
+        self.assertTrue(AMAZON_ASSOC_TAG, "Amazon associated tag is empty")
+    
 
 class InitialTest(unittest.TestCase):
     """Test how it works out from the box"""
