8957de1fdee97ad5e66deb8487ac661bb9b88085
annelida <viqsiq@gmail.com>
2017-06-05 15:02:26 +0300

    Python square calculating function

---
+++ square.py
@@ -0,0 +1,16 @@
+def square(x):
+    """Return the square of x.
+
+    >>> square(2)
+    4
+    >>> square(-2)
+    4
+    >>> square(2.0)
+    4
+    >>> square(-2.0)
+    4
+    >>> square(1j)
+    (-1 + 0j)
+
+    """
+    return x * x
