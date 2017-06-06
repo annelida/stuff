1e1ee60230d2e3ae11d40b3aa7c4e64801f2a36b
annelida <viqsiq@gmail.com>
2017-06-06 14:06:58 +0300

    class attributes declaration

--- projects/2017/amazon/test_amazon_db.py
+++ projects/2017/amazon/test_amazon_db.py
@@ -8,13 +8,22 @@ class TestAmazonDatabase(unittest.TestCase):
     Setup a temporary database
     """
     def setup(self):
-        item = [1, 'B00EOE0WKQ', 'Amazon Fire Phone, 32GB (AT&T)']
-        connection = sqlite3.connect('amazon_data.db')
-        cursor = connection.cursor()
-        cursor.execute("DROP TABLE IF EXISTS amazon_product_data")
-        cursor.execute("CREATE TABLE amazon_product_data(id INTEGER PRIMARY KEY AUTOINCREMENT, asin VARCHAR(12), title TEXT);")
-        cursor.execute("INSERT INTO amazon_product_data VALUES(?, ?, ?);", (item))
-        connection.commit()
-        print("Item stored in database: %s" % (item))
+        self.item = [1, 'B00EOE0WKQ', 'Amazon Fire Phone, 32GB (AT&T)']
+        self.connection = sqlite3.connect('amazon_data.db')
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("DROP TABLE IF EXISTS amazon_product_data")
+        self.cursor.execute("CREATE TABLE amazon_product_data(id INTEGER PRIMARY KEY AUTOINCREMENT, asin VARCHAR(12), title TEXT);")
+        self.cursor.execute("INSERT INTO amazon_product_data VALUES(?, ?, ?);", (self.item))
+        self.connection.commit()
+        print("Item stored in database: %s" % (self.item))
 
-    
+    def test_product_table(self):
+        """
+        Tests product table
+        """
+        print(self.item)
+        #self.assertTrue('amazon_product_data')
+
+
+#    def tearDown(self):
+        
