1fc0f51382e27672cdfd83083f76f2d29955fe8a
annelida <viqsiq@gmail.com>
2017-06-14 07:31:41 +0300

    Python square test

--- square_test.py
+++ square_test.py
@@ -3,22 +3,11 @@ from square import square
 
 
 class SquareTest(unittest.TestCase):
-    def test(self):
+    def test_square(self):
         self.assertEqual(square(2), 4)
-
-    def test_negative(self):
         self.assertEqual(square(-2), 4)
-
-    def test_float(self):
         self.assertEqual(square(2.0), 4)
-
-    def test_float_negative(self):
         self.assertEqual(square(-2.0), 4)
-
-    def test_strings(self):
-        """
-        Test the square of 'str' returns the error
-        """
         with self.assertRaises(TypeError):
             square('2', '-2', '2.0', '-2.0')
 
