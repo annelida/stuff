91626b40d16cbbf6d5a1eaf924254182940a95d6
annelida <viqsiq@gmail.com>
2017-06-23 11:21:48 +0300

    Python, SQlite, unittest using SELECT operation

--- Scripts/test_mydatabase.py
+++ Scripts/test_mydatabase.py
@@ -25,5 +25,16 @@ class TestMyDatabase(unittest.TestCase):
         print("Records created successfully")
         self.conn.commit()
 
+    def test_search_entries(self):
+        cursor = self.conn.execute("SELECT id, name, address, salary from COMPANY")
+        for row in cursor:
+            print "ID = ", row[0]
+            print "NAME = ", row[1]
+            print "ADDRESS = ", row[2]
+            print "SALARY = ", row[3], "\n"
+
+        print("Operation done successfully")
+
+
     def tearDown(self):
         os.remove("mydatabase.db")
