c888c09e276d68f5d4479f0f8e1aecf378fe2f91
annelida <viqsiq@gmail.com>
2017-06-23 09:36:19 +0300

    Python, SQlite, unittest records was added in table

--- Scripts/test_mydatabase.py
+++ Scripts/test_mydatabase.py
@@ -14,6 +14,15 @@ class TestMyDatabase(unittest.TestCase):
                  (ID INT PRIMARY KEY   NOT NULL,
                  NAME TEXT NOT NULL, AGE INT NOT NULL, ADDRESS CHAR(50),SALARY         REAL);''')
         print("Table created successfully")
+        conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+              VALUES (1, 'Paul', 32, 'California', 20000.00 )")
+        conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+              VALUES (2, 'Allen', 25, 'Texas', 15000.00 )")
+        conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+              VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )")
+        conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+              VALUES (4, 'Mark', 25, 'Rich-Mond', 65000.00 )")
+        print("Records created successfully")
         conn.close()
 
     def tearDown(self):
