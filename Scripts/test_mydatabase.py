08b93097c97ab85735a920230c2c09237ea6531d
annelida <viqsiq@gmail.com>
2017-06-23 11:20:09 +0300

    Class attribute was added

--- Scripts/test_mydatabase.py
+++ Scripts/test_mydatabase.py
@@ -6,24 +6,24 @@ import sqlite3
 class TestMyDatabase(unittest.TestCase):
 
     def setUp(self):
-        conn = sqlite3.connect("mydatabase.db")
+        self.conn = sqlite3.connect("mydatabase.db")
         print("Opened database successfully")
 
-        conn.execute("DROP TABLE IF EXISTS COMPANY")
-        conn.execute('''CREATE TABLE COMPANY
+        self.conn.execute("DROP TABLE IF EXISTS COMPANY")
+        self.conn.execute('''CREATE TABLE COMPANY
                  (ID INT PRIMARY KEY   NOT NULL,
                  NAME TEXT NOT NULL, AGE INT NOT NULL, ADDRESS CHAR(50),SALARY         REAL);''')
         print("Table created successfully")
-        conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        self.conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
               VALUES (1, 'Paul', 32, 'California', 20000.00 )")
-        conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        self.conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
               VALUES (2, 'Allen', 25, 'Texas', 15000.00 )")
-        conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        self.conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
               VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )")
-        conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        self.conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
               VALUES (4, 'Mark', 25, 'Rich-Mond', 65000.00 )")
         print("Records created successfully")
-        conn.close()
+        self.conn.commit()
 
     def tearDown(self):
         os.remove("mydatabase.db")
