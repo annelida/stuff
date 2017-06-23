4143a7b2ff9d1e91f53516e0554e4563eafbc5fe
annelida <viqsiq@gmail.com>
2017-06-23 08:12:42 +0300

    Python, unittest, SQlite creating a table

--- Scripts/test_mydatabase.py
+++ Scripts/test_mydatabase.py
@@ -7,7 +7,14 @@ class TestMyDatabase(unittest.TestCase):
 
     def setUp(self):
         conn = sqlite3.connect("mydatabase.db")
-    print("Opened database successfully")
+        print("Opened database successfully")
+
+        conn.execute("DROP TABLE IF EXISTS COMPANY")
+        conn.execute('''CREATE TABLE COMPANY
+                 (ID INT PRIMARY KEY   NOT NULL,
+                 NAME TEXT NOT NULL, AGE INT NOT NULL, ADDRESS CHAR(50),SALARY         REAL);''')
+        print("Table created successfully")
+        conn.close()
 
     def tearDown(self):
         os.remove("mydatabase.db")
