c25692a3ce6ddf56ddc8047533498e4df602c6b2
Viktoria Kosinov <vikky@localhost.localdomain>
2017-07-11 07:41:07 +0300

    Org-babel settings was added

--- Emacs/init_acer.el
+++ Emacs/init_acer.el
@@ -20,22 +20,24 @@
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 ;;
 (global-set-key (kbd "C-x g") 'magit-status)
-;; (org-babel-do-load-languages
-;;  'org-babel-load-languages
-;;  '((python . t)
-;;    (emacs-lisp . t)
-;;    (shell . t)
-;;    (js . t)
-;;    (ditaa . t)
-;;    (plantuml . t)
-;;    (sqlite . t)
-;;    ))
-;; ;; ^^^ Babel customization
+;;
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)
+   (emacs-lisp . t)
+   (sh . t)
+   (js . t)
+   (ditaa . t)
+   (plantuml . t)
+   (sqlite . t)
+   ))
+;;
+;; ^^^ Babel customization
 ;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
+;; custom-set-variables was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
 ;;  '(Info-default-directory-list (quote ("/usr/share/info/emacs-25/")))
 ;;  '(org-agenda-files
 ;;    (quote
