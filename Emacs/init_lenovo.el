ec69f8bbdf2012b21fec1d49ef6a17ca616e2753
annelida <viqsiq@gmail.com>
2017-07-26 09:21:16 +0300

    Magit global key was added

--- Emacs/init_lenovo.el
+++ Emacs/init_lenovo.el
@@ -14,9 +14,10 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 ;;
+(global-set-key (kbd "C-x g") 'magit-status)
 ;; (package-initialize)
 ;; ;;
-;; (org-babel-do-load-languages
+;;(org-babel-do-load-languages
 ;;  'org-babel-load-languages
 ;;  '((python . t)
 ;;    (emacs-lisp . t)
