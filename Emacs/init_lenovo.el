f1f5048a37eb181292fdf3e61988258ae049fef4
annelida <viqsiq@gmail.com>
2017-07-27 16:45:15 +0300

    Google-translate was installed

--- Emacs/init_lenovo.el
+++ Emacs/init_lenovo.el
@@ -27,7 +27,7 @@
    (sqlite . t)
    ))
 ;;
-;; (package-initialize)
+(package-initialize)
 ;; ;;
 ;;(org-babel-do-load-languages
 ;;  'org-babel-load-languages
@@ -113,4 +113,9 @@
 ;;
 (desktop-save-mode 1)
 ;;
-
+(require 'google-translate)
+(require 'google-translate-default-ui)
+(global-set-key "\C-ct" 'google-translate-at-point)
+(global-set-key "\C-cT" 'google-translate-query-translate)
+(setq google-translate-default-source-language ' "en")
+(setq google-translate-default-target-language ' "ru")
