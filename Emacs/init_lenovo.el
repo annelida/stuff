75180230d09e536f257cb3a289808b93c5c48f01
annelida <viqsiq@gmail.com>
2017-08-31 08:54:50 +0300

    org-drill settings

--- Emacs/init_lenovo.el
+++ Emacs/init_lenovo.el
@@ -86,3 +86,19 @@
 (global-set-key "\C-cT" 'google-translate-query-translate)
 (setq google-translate-default-source-language ' "en")
 (setq google-translate-default-target-language ' "ru")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-drill))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;;
+(require 'org-drill)
